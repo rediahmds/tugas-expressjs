6d30a57629404fede3ead75387a8afb603563fa8
Redi Ahmad <ahmadredi67@gmail.com>
2023-05-12 21:40:53 +0700

    feat: :sparkles: handle POST request on `/biodata` endpoint
    Client can now send data in form of JSON to `/biodata` endpoint via POST request

--- server.js
+++ server.js
@@ -1,17 +1,15 @@
 require('dotenv').config();
 const express = require('express');
-const { isEmpty } = require('./lib');
+const { isEmpty } = require('./src/lib');
+const biodata = require('./src/biodata');
 
 const app = express();
 const port = process.env.PORT || 9000;
-const defaultBiodata = {
-  nama: 'Redi Ahmad',
-  domisili: 'Tangerang, Banten',
-};
 
 app.listen(port, () => console.log(`Server running on port: ${port}`));
 
-app.use(express.json());
+app.use(express.json()); // enables to send data in JSON format
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
   res.send('<h1>This shall be an important endpoint</h1>');
@@ -22,7 +20,7 @@ app.get('/', (req, res) => {
 app.get('/biodata', (req, res) => {
   // if no query, response with default biodata
   if (isEmpty(req.query)) {
-    res.send(defaultBiodata);
+    res.send(biodata);
   }
 
   const { name, alamat } = req.query;
@@ -33,4 +31,20 @@ app.get('/biodata', (req, res) => {
     'tanggal-lahir': req.query['tanggal-lahir'],
   };
   res.send(queryResult);
+});
+
+app.post('/biodata', (req, res) => {
+  const dataSentByClient = req.body;
+  const biodataInitialLength = biodata.length;
+  biodata.push(dataSentByClient);
+  const biodataNewLength = biodata.length;
+
+  // Send post request result
+  res.send({
+    data: dataSentByClient,
+    result:
+      biodataInitialLength !== biodataNewLength
+        ? 'Successfully send the data :D'
+        : 'Failed to send the data :(',
+  });
 });
