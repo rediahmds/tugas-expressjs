e9b81c4e709de7799900d1cc5db3b2dfe973e973
Redi Ahmad <ahmadredi67@gmail.com>
2023-05-13 15:18:07 +0700

    fix: :bug: query result and validate the data sent via POST request
    - The query result is now fixed. It also now has the abilitity to find the result even with only few
    properties provide
    
    - The POST request will validate the request body. It will check the whole properties

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const { isEmpty } = require('./src/lib');
 const biodata = require('./src/biodata');
+const equal = require('fast-deep-equal');
 
 const app = express();
 const port = process.env.PORT || 9000;
@@ -12,39 +13,66 @@ app.use(express.json()); // enables to send data in JSON format
 app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-  res.send('<h1>This shall be an important endpoint</h1>');
+  res.send(`<h1>This shall be an important endpoint</h1>
+  <span> Try to visit <code>/biodata</code> endpoint and do some query or POST request</span>`);
 });
 
 // /biodata endpoint
 // This covers minimum requirement for the assignment
 app.get('/biodata', (req, res) => {
-  // if no query, response with default biodata
-  if (isEmpty(req.query)) {
-    res.send(biodata);
+  // if no query, response with all biodata
+  if (isEmpty(req.query)) return res.send(biodata);
+
+  // Logic for alkademi assignment
+  const alkademiQuery =
+    req.query.name &&
+    req.query.alamat &&
+    req.query['tempat-lahir'] &&
+    req.query['tanggal-lahir'];
+
+  if (alkademiQuery) {
+    const alkademiQueryObj = req.query;
+    biodata.push(alkademiQueryObj); // this query also add new data
+    return res.send(alkademiQueryObj);
   }
 
-  const { name, alamat } = req.query;
-  const queryResult = {
-    name,
-    alamat,
-    'tempat-lahir': req.query['tempat-lahir'],
-    'tanggal-lahir': req.query['tanggal-lahir'],
-  };
-  res.send(queryResult);
+  const queryResult = biodata.filter(personBiodata => {
+    for (const queryKey in req.query) {
+      if (Object.hasOwn(personBiodata, queryKey)) {
+        if (personBiodata[queryKey] === req.query[queryKey]) {
+          return personBiodata;
+        } else continue;
+      }
+    }
+  });
+
+  if (!queryResult && !isEmpty(req.query))
+    return res.status(404).send(`Ooops! No results matched with your query`);
+  return res.send({
+    query: req.query,
+    result: queryResult,
+  });
 });
 
 app.post('/biodata', (req, res) => {
-  const dataSentByClient = req.body;
-  const biodataInitialLength = biodata.length;
-  biodata.push(dataSentByClient);
-  const biodataNewLength = biodata.length;
+  const { nama, alamat, tempat_lahir, tanggal_lahir } = req.body;
 
   // Send post request result
-  res.send({
-    data: dataSentByClient,
-    result:
-      biodataInitialLength !== biodataNewLength
-        ? 'Successfully send the data :D'
-        : 'Failed to send the data :(',
-  });
+
+  // failed to post the request body
+  const validatePostBody =
+    nama === '' || !nama || !alamat || !tempat_lahir || !tanggal_lahir;
+  if (validatePostBody) {
+    res.status(400).send({
+      data: req.body,
+      result: 'Failed to send the data :( Have you check the properties?',
+    });
+  } else {
+    // successfully post the data
+    biodata.push(req.body);
+    res.status(201).send({
+      data: req.body,
+      result: 'Successfully send the data :D',
+    });
+  }
 });
